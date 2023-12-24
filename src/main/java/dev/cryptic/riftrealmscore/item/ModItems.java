bdd6a2c7f8894793cbfa54f7c5226adaaddbfb7f
BuildTools <unconfigured@null.spigotmc.org>
2023-12-23 19:47:57 -0800

    CustomSwordItem.java added Neat in gradle made neater

--- src/main/java/dev/cryptic/riftrealmscore/item/ModItems.java
+++ src/main/java/dev/cryptic/riftrealmscore/item/ModItems.java
@@ -1,6 +1,7 @@
 package dev.cryptic.riftrealmscore.item;
 
 import dev.cryptic.riftrealmscore.RiftRealmsCore;
+import dev.cryptic.riftrealmscore.item.custom.CustomSwordItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,12 +18,13 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
-    public static final RegistryObject<Item> WEEE_SWORD = ITEMS.register("weee_sword",
-            () -> new UniqueSwordItem(Tiers.DIAMOND,9,1.9F,
-            new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static final RegistryObject<Item> MY_AWESOME_SWORD = ITEMS.register("my_awesome_sword",
-            () -> new UniqueSwordItem(Tiers.NETHERITE, 3, 1.3F,
+            () -> new UniqueSwordItem(Tiers.NETHERITE, 30, 1.3F,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> MY_EVEN_BETTER_SWORD = ITEMS.register("my_best_sword",
+            () -> new CustomSwordItem( 50, -2.4F,
                     new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus) {
---
+++ src/main/java/dev/cryptic/riftrealmscore/item/custom/CustomSwordItem.java
@@ -0,0 +1,83 @@
+package dev.cryptic.riftrealmscore.item.custom;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+
+public class CustomSwordItem extends Item {
+    private final float attackDamage;
+    private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
+    public CustomSwordItem(float attackDamage, float attackSpeed, Properties properties) {
+        super(properties);
+
+        this.attackDamage = attackDamage;
+
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", (double)this.attackDamage, AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", (double)attackSpeed, AttributeModifier.Operation.ADDITION));
+        this.defaultModifiers = builder.build();
+    }
+
+    public float getDamage() {
+        return this.attackDamage;
+    }
+
+    public boolean canAttackBlock(BlockState blockState, Level level, BlockPos blockPos, Player player) {
+        return !player.isCreative();
+    }
+
+    public float getDestroySpeed(ItemStack itemStack, BlockState blockState) {
+        if (blockState.is(Blocks.COBWEB)) {
+            return 15.0F;
+        } else {
+            Material material = blockState.getMaterial();
+            return material != Material.PLANT && material != Material.REPLACEABLE_PLANT && !blockState.is(BlockTags.LEAVES) && material != Material.VEGETABLE ? 1.0F : 1.5F;
+        }
+    }
+
+    public boolean hurtEnemy(ItemStack itemStack, LivingEntity entity, LivingEntity entity1) {
+        itemStack.hurtAndBreak(1, entity1, (e) -> {
+            e.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+        return true;
+    }
+
+    public boolean mineBlock(ItemStack itemStack, Level level, BlockState blockState, BlockPos blockPos, LivingEntity entity) {
+        if (blockState.getDestroySpeed(level, blockPos) != 0.0F) {
+            itemStack.hurtAndBreak(2, entity, (e) -> {
+                e.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+            });
+        }
+
+        return true;
+    }
+
+    public boolean isCorrectToolForDrops(BlockState blockState) {
+        return blockState.is(Blocks.COBWEB);
+    }
+
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot slot) {
+        return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(slot);
+    }
+
+    @Override
+    public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+        return ToolActions.DEFAULT_SWORD_ACTIONS.contains(toolAction);
+    }
+}
